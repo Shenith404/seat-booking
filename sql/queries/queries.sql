48236eccddd74bbcced7ff2c857a55b12c113f57
Shenith Bandara <139686889+Shenith404@users.noreply.github.com>
2026-03-31 18:15:41 +0530

    create quireis

--- sql/queries/queries.sql
+++ sql/queries/queries.sql
@@ -1,21 +1,21 @@
 -- name: GetShowSeats :many
-SELECT id, hall_id, row_name, seat_number 
-FROM seats 
-WHERE hall_id = (SELECT hall_id FROM shows WHERE id = $1);
+SELECT s.id, s.hall_id, s.row_name, s.seat_number 
+FROM seats s
+WHERE s.hall_id = (SELECT sh.hall_id FROM shows sh WHERE sh.id = $1);
 
 -- name: CheckSeatsAvailableForUpdate :many
 -- This locks the rows so no one else can book them while we check
-SELECT id 
-FROM tickets 
-WHERE show_id = $1 AND seat_id = ANY($2::UUID[])
+SELECT t.id 
+FROM tickets t
+WHERE t.show_id = $1 AND t.seat_id = ANY($2::UUID[])
 FOR UPDATE;
 
 -- name: CreateBooking :one
-INSERT INTO bookings (show_id, customer_email, customer_phone, status)
-VALUES ($1, $2, $3, 'completed')
-RETURNING booking_id,customer_email,customer_phone,status,created_at;
+INSERT INTO booking (id, show_id, customer_email, customer_phone, status)
+VALUES (gen_random_uuid(), $1, $2, $3, 'completed')
+RETURNING id, show_id, customer_email, customer_phone, status, created_at;
 
 -- name: CreateTicket :one
-INSERT INTO tickets (booking_id, show_id, seat_id, qr_code_hash)
-VALUES ($1, $2, $3, $4)
-RETURNING id;
+INSERT INTO tickets (id, booking_id, show_id, seat_id, qr_code_hash)
+VALUES (gen_random_uuid(), $1, $2, $3, $4)
+RETURNING id, booking_id, show_id, seat_id, qr_code_hash, created_at;
